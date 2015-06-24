0fed306bd2f96fff013124b078faeeb305b5ae51
Alan W George <ageorge@alan-george.com>
2015-06-24 14:19:12 -0700

    sync

--- app/src/main/scala/com/alangeorge/android/scalaapp/MainScalaActivity.scala
+++ app/src/main/scala/com/alangeorge/android/scalaapp/MainScalaActivity.scala
@@ -2,11 +2,19 @@ package com.alangeorge.android.scalaapp
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.Menu
 import android.view.MenuItem
 
+object MainScalaActivity {
+  private val TAG: String = "MainScalaActivity"
+}
+
 class MainScalaActivity extends AppCompatActivity {
+  import MainScalaActivity._
+
   protected override def onCreate(savedInstanceState: Bundle) {
+    Log.d(TAG, "onCreate")
     super.onCreate(savedInstanceState)
     setContentView(R.layout.activity_main)
   }
--- app/src/main/scala/com/alangeorge/android/scalaapp/MainScalaActivityFragment.scala
+++ app/src/main/scala/com/alangeorge/android/scalaapp/MainScalaActivityFragment.scala
@@ -2,16 +2,20 @@ package com.alangeorge.android.scalaapp
 
 import android.support.v4.app.Fragment
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 
-/**
- * A placeholder fragment containing a simple view.
- */
+object MainScalaActivityFragment {
+  private val TAG: String = "MainScalaActivityFragment"
+}
+
 class MainScalaActivityFragment extends Fragment {
+  import MainScalaActivityFragment._
 
   override def onCreateView(inflater: LayoutInflater, container: ViewGroup, savedInstanceState: Bundle): View = {
+    Log.d(TAG, "onCreateView")
     inflater.inflate(R.layout.fragment_main, container, false)
   }
 }
